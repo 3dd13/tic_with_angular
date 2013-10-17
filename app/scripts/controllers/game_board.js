cb6f993fa6b18179b914dc6349e09bb09e2a77e5
Eddie Lau <hi@3dd13.me>
2013-10-17 16:15:01 +0800

    include latest game board communication

--- app/scripts/controllers/game_board.js
+++ app/scripts/controllers/game_board.js
@@ -4,65 +4,65 @@ angular.module('ticWithAngularFireApp')
   .controller('GameBoardCtrl', function ($scope, $routeParams, angularFire) {
     $scope.gameBoardId = $routeParams.id;
     $scope.mySymbol = $routeParams.mySymbol;    
+    $scope.myTurn = false;
     
-    $scope.gameBoard = [];
-    var gameBoardRef = new Firebase("https://example.firebaseio.com/room/" + $routeParams.id);
-    $scope.promise = angularFire(gameBoardRef, $scope, "gameBoard");
+    // $scope.gameBoard = [];
+    var gameBoardRef = new Firebase("https://3dd13-ttt-game.firebaseio.com/room/" + $routeParams.id);
+    $scope.promise = angularFire(gameBoardRef, $scope, "gameBoard", []);
 
-    /*
-      Step 1
-    */
-    // $scope.promise.then (function () {
-    //   console.log("In Game Board");
-    //   console.log($scope.gameBoardId);
-    //   console.log($scope.mySymbol);      
-    // });
-    
-    /*
-      Step 2
-    */
     $scope.promise.then (function () {
-      if ($scope.gameBoard.length == 0 && $routeParams.mySymbol == 'x') {
+      $scope.gameBoard = [];
+      if ($routeParams.mySymbol == 'x') {
         console.log("I am First Move: Symbol: " + $routeParams.mySymbol);
-        $scope.makeMyMove();
+        $scope.myTurn = true;
       } else {
         console.log("I am Second Move: Symbol: " + $routeParams.mySymbol);
-        $scope.waitForOpponentToMove();
+        $scope.myTurn = false;
       }
     });
     
-    $scope.waitForOpponentToMove = function() {
-      gameBoardRef.on('child_added', function(snapshot) {
-        gameBoardRef.off('child_added');
-        
-        if ($scope.isLosing()) {
-          // print losing
+    gameBoardRef.on('value', function(snapshot) {
+      console.log("wait received");
+      if (!$scope.myTurn) {
+        if (snapshot.val() != null) {
+          if (!arrays_equal(snapshot.val(), $scope.gameBoard)) {
+            console.log("diff gameboard");
+            if ($scope.isLosing()) {
+              // print losing
+              // redirect to match player if play again
+            } else if ($scope.isDraw()) {
+              // print draw
+              // redirect to match player if play again
+            } else {
+              $scope.myTurn = true;
+            }
+          } else {
+            console.log("same gameboard"); 
+          }
+        } else {
+          console.log("snapshot is empty");
+        }
+      } else {
+        console.log("it is my turn but I receive ");
+      }
+    });
+    
+
+            
+    $scope.handleClick = function(index) {
+      if ($scope.myTurn) {
+        $scope.gameBoard[index] = $scope.mySymbol;
+      
+        if ($scope.isWinning()) {
+          // print winning
           // redirect to match player if play again
         } else if ($scope.isDraw()) {
           // print draw
           // redirect to match player if play again
         } else {
-          $scope.makeMyMove();
+          $scope.myTurn = false;
         }
-      });
-    };
-    
-    $scope.makeMyMove = function() {
-      $scope.listenForMyClick();
-      
-      if ($scope.isWinning()) {
-        // print winning
-        // redirect to match player if play again
-      } else if ($scope.isDraw()) {
-        // print draw
-        // redirect to match player if play again
-      } else {
-        $scope.waitForOpponentToMove();
       }
-    }
-
-    $scope.listenForMyClick = function() {
-      // handle click event on cell 
     }
     
     $scope.isLosing = function() {
@@ -76,4 +76,6 @@ angular.module('ticWithAngularFireApp')
     $scope.isDraw = function() {
       return false; 
     }    
+    
+    function arrays_equal(a,b) { return !(a<b || b<a); }
   });
